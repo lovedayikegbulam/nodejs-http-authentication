442fd41964c531a304783b7446cb5a0cff27ef88
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-01-28 13:42:09 +0100

    add authentication file and function

---
+++ authenticate.js
@@ -0,0 +1,22 @@
+const { findUser } = require("./dbHandlers.js");
+
+function authenticate(req, res, next) {
+	const { username, password } = req.headers;
+	console.log("authenticate", req.headers);
+	const user = findUser(req.headers.username);
+	if (!user) {
+		res.statusCode = 401;
+		res.end();
+		return;
+	}
+	if (user.username !== username || user.password !== password) {
+		res.statusCode = 401;
+		res.end();
+		return;
+	}
+	next(req, res);
+}
+
+module.exports = {
+    authenticate
+}
