a1c1661d9559f56389c85edf5e7e65ee29d90ea7
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-01-28 11:22:01 +0100

    add api routes with appropriate http methods

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ const port = 8000;
 const { findUser } = require("./dbHandlers");
 
 
-// Add Request Listener to the server
-const requestHandler = function (request, response) {
-	response.writeHead(200); // Status code 200 = OK
-	response.write("server is working correctly");
-	response.end();
+// Add Request Handler to the server
+const requestHandler = function (req, res) {
+	res.setHeader("Content-Type", "application/json");
+
+	if (req.url === "/books" && req.method === "GET") {
+		res.end("Hello from /GET Books");
+	} else if (req.url === "/books" && req.method === "POST") {
+		res.end("Hello from /POST Books");
+	} else if (req.url === "/books" && req.method === "PUT") {
+		res.end("Hello from /PUT Books");
+	} else if (req.url === "/books" && req.method === "PATCH") {
+		res.end("Hello from /PATCH Books");
+	} else if (req.url === "/books" && req.method === "DELETE") {
+		res.end("Hello from /DELETE Books");
+	} else {
+		res.writeHead(404);
+		res.end(
+			JSON.stringify({
+				message: "Method Not Supported",
+			})
+		);
+	}
 };
 
 // Create the server
