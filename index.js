c56d664b606c97c17b55977815eb0b4732b5c47f
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-01-28 12:23:15 +0100

    get and update header data in the reqhandler func

--- index.js
+++ index.js
@@ -4,29 +4,30 @@ const hostname = "localhost";
 const port = 8000;
 
 const { findUser } = require("./dbHandlers");
-
+const { getBodyFromStream } = require("./buffer");
 
 // Add Request Handler to the server
-const requestHandler = function (req, res) {
+const requestHandler = async function (req, res) {
 	res.setHeader("Content-Type", "application/json");
 
-	if (req.url === "/books" && req.method === "GET") {
-		res.end("Hello from /GET Books");
-	} else if (req.url === "/books" && req.method === "POST") {
-		res.end("Hello from /POST Books");
-	} else if (req.url === "/books" && req.method === "PUT") {
-		res.end("Hello from /PUT Books");
-	} else if (req.url === "/books" && req.method === "PATCH") {
-		res.end("Hello from /PATCH Books");
-	} else if (req.url === "/books" && req.method === "DELETE") {
-		res.end("Hello from /DELETE Books");
-	} else {
-		res.writeHead(404);
-		res.end(
-			JSON.stringify({
-				message: "Method Not Supported",
-			})
-		);
+	try {
+		const header = await getBodyFromStream(req);
+		req.headers = header;
+
+		if (req.url === "/books" && req.method === "GET") 
+            console.log(req.headers);
+            res.end("Hello from /GET Books");
+		if (req.url === "/books" && req.method === "POST")
+			res.end("Hello from /POST Books");
+		if (req.url === "/books" && req.method === "PUT")
+			res.end("Hello from /PUT Books");
+		if (req.url === "/books" && req.method === "PATCH")
+			res.end("Hello from /PATCH Books");
+		if (req.url === "/books" && req.method === "DELETE")
+			res.end("Hello from /DELETE Books");
+	} catch (error) {
+		res.statusCode = 500;
+		res.end(error.message);
 	}
 };
 
